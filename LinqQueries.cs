c6b32ee8e845c8ee427045406b769f5adbd97798
Santiago Rojas <santirojastr12@gmail.com>
2023-08-10 19:35:23 -0500

    Importacion de datos desde el archivo json hacia el programa

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,4 +1,17 @@
 public class LinqQueries
 {
-    public LinqQueries() { }
+    private List<Book> booksCollection = new List<Book>();
+    public LinqQueries()
+    {
+        using (StreamReader reader = new StreamReader("books.json"))
+        {
+            string json = reader.ReadToEnd();
+            this.booksCollection = System.Text.Json.JsonSerializer.Deserialize<List<Book>>(json, new System.Text.Json.JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+    }
+
+    public IEnumerable<Book> AllCollection()
+    {
+        return booksCollection;
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿LinqQueries linqQueries = new LinqQueries();
+
+PrintValues(linqQueries.AllCollection());
+
+void PrintValues(IEnumerable<Book> booksList)
+{
+    Console.WriteLine("{0, -60} {1, 15} {2, 15}\n", "Title", "Page Count", "Published Date");
+    foreach (var book in booksList)
+    {
+        Console.WriteLine("{0, -60} {1, 15} {2, 15}", book.Title, book.PageCount, book.PublishedDate.ToShortDateString());
+    }
+}
